67c514b6ceb4ddc35c8794268dd1e15910cc3692
ImMohammadHosseini <sayedmh50@gmail.com>
2023-04-27 16:11:05 +0330

    add ppo_config class

---
+++ configs/ppo_configs.py
@@ -0,0 +1,15 @@
+
+"""
+
+"""
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class PPOConfig(object):
+    """
+    this implementation is inspired by: https://github.com/lvwerra/trl/blob/main/trl/trainer/ppo_config.py
+    Configuration class for PPOTrainer
+    """
+    pass
