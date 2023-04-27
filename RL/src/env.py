5b3d1a0df27b49a232de47c955deab91bfd75a09
ImMohammadHosseini <sayedmh50@gmail.com>
2023-04-27 16:10:14 +0330

    modified

--- RL/src/env.py
+++ RL/src/env.py
@@ -2,12 +2,18 @@
 """
 
 """
+import torch
+from typing import Callable, List, Optional, Union
+from datasets import Dataset
 from torchrl.envs import (
     EnvBase,
 )
 
 class KnapsackAssignmentEnv(EnvBase):
-    def __init__ (self):
+    def __init__ (
+            self,
+            dataset: Optional[Union[torch.utils.data.Dataset, Dataset]] = None,
+            ):
         pass
     def _reset (self):
         pass
