3cfa4276a9cdf61dc8b18ac4f807bfd07c547dd5
ImMohammadHosseini <sayedmh50@gmail.com>
2023-04-27 16:06:11 +0330

    add RL/src and change dir

---
+++ RL/src/env.py
@@ -0,0 +1,18 @@
+
+"""
+
+"""
+from torchrl.envs import (
+    EnvBase,
+)
+
+class KnapsackAssignmentEnv(EnvBase):
+    def __init__ (self):
+        pass
+    def _reset (self):
+        pass
+    def _step (self):
+        pass
+    
+    def _set_seed (self):
+        pass
