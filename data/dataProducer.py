bc33e2dd019310f5eef550f44ce7f0c368934ae0
ImMohammadHosseini <sayedmh50@gmail.com>
2023-04-30 08:52:58 +0330

    add multiObjectiveDimentional method for producing data

--- data/dataProducer.py
+++ data/dataProducer.py
@@ -20,5 +20,16 @@ def multipleKnapSackData (M:int, N:int):
 def multiDimentional (M:int, N:int):
     pass
 
-def multiObjectiveDimentional (M:int, N:int):
-    pass
+def multiObjectiveDimentional (d:int, M:int, N:int):
+    """
+    multi-Objective multi-Dimentional multipleKnapSack, 
+    multi capacities for each knapsack, 
+    multi weight and one value for each instance
+    set a random dataset for M knapsack and N instance
+    produce knapsack capacity array in size of (d,M), instance weight array 
+    in size of (d,N)  and instance value array in size of N
+    """
+    capacities = np.random.randint(low=5, high=200, size=(d,M))
+    weights = np.random.randint(low=2, high=100, size=(d,N))
+    values = np.random.randint(low=1, high=200, size=N)
+    return capacities, weights, values
