f90d24392786f959ec261a50416c6ed04f474b36
ImMohammadHosseini <sayedmh50@gmail.com>
2023-05-07 12:44:01 +0330

    copy LengthSampler, ref:https://github.com/lvwerra/trl/blob/main/trl/core.py#L235

--- RL/src/core.py
+++ RL/src/core.py
@@ -12,3 +12,14 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+class LengthSampler:
+    """
+    Samples a length
+    """
+
+    def __init__(self, min_value, max_value):
+        self.values = list(range(min_value, max_value))
+
+    def __call__(self):
+        return np.random.choice(self.values)
