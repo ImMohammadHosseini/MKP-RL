b2d72f48a6b926053a5d997c45f68a0baeeff704
ImMohammadHosseini <sayedmh50@gmail.com>
2023-05-07 10:39:05 +0330

    modified

--- data/dataProducer.py
+++ data/dataProducer.py
@@ -5,22 +5,25 @@
 import numpy as np
 
 
-def multipleKnapSackData (M:int, N:int):
+def multipleKnapSackData (M:int, N:int, info):
     """
     multipleKnapSack, one capacities for each knapsack, one weight for each instance
     set a random dataset for M knapsack and N instance
     produce knapsack capacity array in size of M, instance weight array 
     in size of N  and instance value array in size of N
     """
-    capacities = np.random.randint(low=5, high=150, size=M)
-    weights = np.random.randint(low=2, high=100, size=N)
-    values = np.random.randint(low=1, high=200, size=N)
+    capacities = np.random.randint(low=info['CAP_LOW'], high=info['CAP_HIGH'],
+                                   size=M)
+    weights = np.random.randint(low=info['WEIGHT_LOW'], high=info['WEIGHT_HIGH'],
+                                size=N)
+    values = np.random.randint(low=info['VALUE_LOW'], high=info['VALUE_HIGH'],
+                               size=N)
     return capacities, weights, values
 
 def multiDimentional (M:int, N:int):
     pass
 
-def multiObjectiveDimentional (D:int, M:int, N:int):
+def multiObjectiveDimentional (D:int, M:int, N:int, info):
     """
     multi-Objective multi-Dimentional multipleKnapSack, 
     multi capacities for each knapsack, 
@@ -29,7 +32,10 @@ def multiObjectiveDimentional (D:int, M:int, N:int):
     produce knapsack capacity array in size of (d,M), instance weight array 
     in size of (d,N)  and instance value array in size of N
     """
-    capacities = np.random.randint(low=5, high=200, size=(M,D))
-    weights = np.random.randint(low=2, high=100, size=(N,D))
-    values = np.random.randint(low=1, high=200, size=N)
+    capacities = np.random.randint(low=info['CAP_LOW'], high=info['CAP_HIGH'], 
+                                   size=(M,D))
+    weights = np.random.randint(low=info['WEIGHT_LOW'], high=info['WEIGHT_HIGH'], 
+                                size=(N,D))
+    values = np.random.randint(low=info['VALUE_LOW'], high=info['VALUE_HIGH'], 
+                               size=N)
     return capacities, weights, values
