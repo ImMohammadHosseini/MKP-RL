0b46bf89f1aa6c0fe2ddcfea91f52a0f40bac689
ImMohammadHosseini <sayedmh50@gmail.com>
2023-05-10 14:25:40 +0330

    delete comments

--- configs/transformers_model_configs.py
+++ configs/transformers_model_configs.py
@@ -5,8 +5,6 @@
 
 from dataclasses import dataclass
 
-
-
 @dataclass
 class TransformerKnapsackConfig (object):
     model_type = "TransformerKnapsack"
@@ -22,11 +20,6 @@ class TransformerKnapsackConfig (object):
         num_decoder_layers: int = 2,
         dropout: float = 0.1,
         batch_first: bool = True,
-        
-        #category_embed_size: int = 32,#delete
-        #pose_embed_size: int = 128,#delete
-        #temporal_embed_size: int = 64,#delete
-        #marker_embed_size: int = 32,#delete
         **kwargs,
     ) -> None:
         self.inst_obs_size = inst_obs_size
@@ -40,15 +33,5 @@ class TransformerKnapsackConfig (object):
         self.dropout = dropout
         self.num_encoder_layers = num_encoder_layers
         self.num_decoder_layers = num_decoder_layers
-        #self.category_embed_size = category_embed_size
-        #self.pose_embed_size = pose_embed_size
-        #self.temporal_embed_size = temporal_embed_size
-        #self.marker_embed_size = marker_embed_size
-        '''assert (
-            category_embed_size
-            + pose_embed_size
-            + temporal_embed_size
-            + marker_embed_size
-            == d_model
-        )'''
+        
         super(TransformerKnapsackConfig, self).__init__(**kwargs)
