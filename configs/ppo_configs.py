3fc14288616fd2a9c0ca5ec6e9df65a4589b2093
ImMohammadHosseini <sayedmh50@gmail.com>
2023-05-15 17:20:06 +0330

    modified

--- configs/ppo_configs.py
+++ configs/ppo_configs.py
@@ -17,12 +17,19 @@ class PPOConfig(object):
         self,
         generat_link_number: int = 100,
         internal_batch: int = 20,
+        ppo_batch_size: int = 5,
+        ppo_epochs: int = 4,
+        gamma: float = 0.99,
+        gae_lambda: float = 0.95,
         
     ):
         assert generat_link_number >= internal_batch
         self.generat_link_number = generat_link_number #number of links generated in on external observation
         self.internal_batch = internal_batch #number of links generated to optimize model
-        
+        self.ppo_batch_size = ppo_batch_size
+        self.ppo_epochs = ppo_epochs
+        self.gamma = gamma
+        self.gae_lambda = gae_lambda
         
     steps: Optional[int] = field(default=20000, metadata={"help": "Number of training steps"})
     learning_rate: Optional[float] = field(default=1e-5, metadata={"help": "Adam learning rate"})
