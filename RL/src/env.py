6408d99fda7ea2790626a4f3d4d1b3713f4d09bb
ImMohammadHosseini <sayedmh50@gmail.com>
2023-05-22 08:56:53 +0330

    modified

--- RL/src/env.py
+++ RL/src/env.py
@@ -51,6 +51,7 @@ class KnapsackAssignmentEnv (gym.Env):
         )
         
         super().__init__()
+        self.len_observation = self.observation_space["knapsack"].shape[0]+self.observation_space["instance_value"].shape[0]+3
         self.device = device
         self.statePrepare = state_dataClass
         #self.statePrepare.normalizeData(info['CAP_HIGH'], info['VALUE_HIGH'])
@@ -76,10 +77,23 @@ class KnapsackAssignmentEnv (gym.Env):
                                                        ACT, axis=0),axis=0),
                                                        axis=0)
         info = self._get_info()
+        if externalObservation.shape[0] < self.len_observation:
+            externalObservation = self._pad_left(externalObservation, self.len_observation,
+                                                 ACT)
         externalObservation = torch.tensor(externalObservation, 
                                            dtype=torch.float32, 
                                            device=self.device).unsqueeze(dim=0)        
         return externalObservation, info
+    
+    def _pad_left(
+        self,
+        sequence: np.ndarray, 
+        final_length: int, 
+        padding_token: np.ndarray,
+    ):
+        self.statePrepare.pad_len = final_length - len(sequence)
+        return np.append(np.repeat(padding_token, (final_length - len(sequence)), 
+                                   axis=0), sequence, axis=0)
     
     def step (self, actions):
         if len(actions) == 0: self.no_change += 1
@@ -96,12 +110,16 @@ class KnapsackAssignmentEnv (gym.Env):
             "instance_value"], ACT, axis=0), np.append(externalObservation["knapsack"], 
                                                        ACT, axis=0),axis=0),
                                                        axis=0)
+        if externalObservation.shape[0] < self.len_observation:
+            externalObservation = self._pad_left(externalObservation, self.len_observation,
+                                                 ACT)
         externalObservation = torch.tensor(externalObservation, 
                                            dtype=torch.float32, 
                                            device=self.device).unsqueeze(dim=0)
         info = self._get_info()
         
         return externalObservation, externalReward, terminated, info
+    
     
     def response_decode (self, responce):
         pass
