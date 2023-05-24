dd44c70bafa597fe0fc4d616163461704ee43809
ImMohammadHosseini <sayedmh50@gmail.com>
2023-05-24 14:23:36 +0330

    add greedy algorithm

--- solve_algorithms/greedy_select.py
+++ solve_algorithms/greedy_select.py
@@ -13,19 +13,46 @@ class GreedySelect():
         state_dataClass: ExternalStatePrepare,
     ):
         self.statePrepare = state_dataClass
-        self.random_num = 3000
+        self.greedy_num = 3000
         self.no_change_long = 1e+3
     
     def _choose_actions_greedy_firstFit (
         self,
     ):
+        accepted_actions = np.zeros((0,2), dtype= int)
+        if (self.statePrepare.is_terminated()):
+            return accepted_actions
         caps, weightValues = self.statePrepare.getObservation()
-
+        greedy_inst_indx = np.flip(np.argsort(weightValues[:, -1]))[:self.greedy_num]
+        for inst_idx in greedy_inst_indx:
+            if inst_idx >= self.statePrepare.pad_len:
+                weight = self.statePrepare.getObservedInstWeight(inst_idx)
+                for ks_idx in range(caps.shape[0]):
+                    knapSack = self.statePrepare.getKnapsack(ks_idx)
+                    eCap = knapSack.getExpectedCap()
+                    if all(eCap >= weight):
+                        knapSack.removeExpectedCap(weight)
+                        accepted_actions = np.append(accepted_actions, 
+                                                     [[inst_idx, ks_idx]], 0)
+                        break
+        return accepted_actions
     
     def test_step (
         self,
     ):
-        pass
+        self.statePrepare.reset()
+        step_acts = np.zeros((0,2), dtype= int)
+        no_change = 0
+        print('choose two random action')
+        while no_change < self.no_change_long:
+            accepted_actions = self._choose_actions_two_random()
+            if len(accepted_actions) == 0:
+               no_change +=1
+               continue
+            step_acts = np.append(step_acts, accepted_actions, 0)
+        self.statePrepare.changeNextState(step_acts)
+        self.score_ratio()
+        
     
     def score_ratio (self):
         score_ratio = 0; remain_cap_ratio = []
