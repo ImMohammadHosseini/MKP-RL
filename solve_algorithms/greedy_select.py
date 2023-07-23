454f0e3c4f9e90f63785b7fc375132a904ab6e4b
ImMohammadHosseini <sayedmh50@gmail.com>
2023-07-23 17:19:00 +0330

    change to use in trainer

--- solve_algorithms/greedy_select.py
+++ solve_algorithms/greedy_select.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 from src.data_structure.state_prepare import ExternalStatePrepare
-
+from typing import Optional
 
 class GreedySelect():
     def __init__ (
@@ -16,46 +16,58 @@ class GreedySelect():
         self.statePrepare = state_dataClass
         self.greedy_num = num_instance
         self.no_change_long = 3
+        self.reset()
     
     def _choose_actions_greedy_firstFit (
         self,
+        pred_num: Optional[int]=None
     ):
         accepted_actions = np.zeros((0,2), dtype= int)
         if (self.statePrepare.is_terminated()):
             return accepted_actions
         caps, weightValues = self.statePrepare.getObservation()
-        greedy_inst_indx = np.flip(np.argsort(weightValues[:, -1]))[:self.greedy_num]
-        for inst_idx in greedy_inst_indx:
-            if inst_idx >= self.statePrepare.pad_len:
-                weight = self.statePrepare.getObservedInstWeight(inst_idx)
-                for ks_idx in range(caps.shape[0]):
-                    ks_act = self.statePrepare.getRealKsAct(ks_idx)
-                    knapSack = self.statePrepare.getKnapsack(ks_act)
-                    eCap = knapSack.getExpectedCap()
-                    if all(eCap >= weight):
-                        knapSack.removeExpectedCap(weight)
-                        accepted_actions = np.append(accepted_actions, 
-                                                     [[inst_idx, ks_act]], 0)
-                        break
+        values = weightValues[:, -1]
+        weightSum = np.sum(weightValues[:, :-1],1)
+        greedy_inst_indx = np.flip(np.argsort(np.trim_zeros(values)/np.trim_zeros(weightSum)))[:self.greedy_num]
+        if pred_num is None:
+            index_range = len(greedy_inst_indx)
+        else:
+            index_range = min(pred_num, len(greedy_inst_indx))
+        for idx in range(index_range):
+            inst_idx = greedy_inst_indx[idx]
+            inst_idx += self.statePrepare.pad_len
+            #if inst_idx >= self.statePrepare.pad_len:
+            weight = self.statePrepare.getObservedInstWeight(inst_idx)
+            for ks_idx in range(caps.shape[0]):
+                ks_act = self.statePrepare.getRealKsAct(ks_idx)
+                knapSack = self.statePrepare.getKnapsack(ks_act)
+                eCap = knapSack.getExpectedCap()
+                if all(eCap >= weight):
+                    knapSack.removeExpectedCap(weight)
+                    accepted_actions = np.append(accepted_actions, 
+                                                 [[inst_idx, ks_act]], 0)
+                    break
         return accepted_actions
     
     def test_step (
         self,
+        pred_num: Optional[int]=None
     ):
-        self.statePrepare.reset()
         step_acts = np.zeros((0,2), dtype= int)
         no_change = 0
-        while no_change < self.no_change_long:
+        #while no_change < self.no_change_long:
             
-            accepted_actions = self._choose_actions_greedy_firstFit()
-            self.statePrepare.changeNextState(accepted_actions)
-            if len(accepted_actions) == 0:
-                no_change +=1
-                continue
-            step_acts = np.append(step_acts, accepted_actions, 0)
+        accepted_actions = self._choose_actions_greedy_firstFit(pred_num)
+        self.statePrepare.changeNextState(accepted_actions)
+        #if len(accepted_actions) == 0:
+        #    no_change +=1
+        #    continue
+        step_acts = np.append(step_acts, accepted_actions, 0)
         return self.score_ratio(), step_acts
         
-    
+    def reset(self):
+        self.statePrepare.reset()
+
     def score_ratio (self):
         score_ratio = 0; remain_cap_ratio = []
         for ks in self.statePrepare.knapsacks:
