81151410031c08f6aae7f68a3fe81935d8427b05
ImMohammadHosseini <sayedmh50@gmail.com>
2023-07-25 13:10:16 +0330

    add method

--- solve_algorithms/RL/src/base_trainer.py
+++ solve_algorithms/RL/src/base_trainer.py
@@ -2,7 +2,8 @@
 """
 
 """
-
+import numpy as np
+from typing import Optional
 
 
 class BaseTrainer ():
@@ -25,7 +26,23 @@ class BaseTrainer ():
 
     def loss (self, *args):
         raise NotImplementedError("Not implemented")
-
+    
+    def generate_batch (
+        self, 
+        n_states: Optional[int] = None,
+        batch_size: Optional[int] = None,
+    ):
+        if batch_size == None:
+            batch_size = self.config.ppo_batch_size
+        if n_states == None:
+            n_states = self.config.internal_batch
+        batch_start = np.arange(0, n_states, batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
+        np.random.shuffle(indices)
+        batches = [indices[i:i+batch_size] for i in batch_start]
+        
+        return batches
+    
     def internal_reward (self, *args):
         raise NotImplementedError("Not implemented")
 
