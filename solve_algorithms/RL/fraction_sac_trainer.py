0440b690727405473537fc2db24d85f65864cc11
ImMohammadHosseini <sayedmh50@gmail.com>
2023-08-07 15:57:47 +0330

    change some methods

--- solve_algorithms/RL/fraction_sac_trainer.py
+++ solve_algorithms/RL/fraction_sac_trainer.py
@@ -78,7 +78,7 @@ class FractionSACTrainer(BaseTrainer):
         self.target_entropy = 0.98 * -np.log(1 / self.actor_model.config.inst_obs_size) * \
             -np.log(1 / self.actor_model.config.knapsack_obs_size)
         self.log_alpha = torch.tensor(np.log(self.config.alpha_initial), requires_grad=True)
-        self.alpha = self.log_alpha
+        self.alpha = self.log_alpha.to(self.device)
         self.alpha_optimiser = AdamW([self.log_alpha], lr=self.config.alpha_lr)
     
         self.savePath = save_path +'/RL/FractionSACTrainer/'
@@ -94,6 +94,7 @@ class FractionSACTrainer(BaseTrainer):
                                                     self.actor_model.config.input_decode_dim))
         self.memory_actions = np.zeros((self.config.buffer_size, 2))
         self.memory_reward = np.zeros(self.config.buffer_size)
+        self.memory_step = np.zeros(self.config.buffer_size)
         self.memory_newObservation = np.zeros((self.config.buffer_size, 
                                                 self.actor_model.config.max_length,
                                                 self.actor_model.config.input_encode_dim))
@@ -106,6 +107,7 @@ class FractionSACTrainer(BaseTrainer):
         actions: np.ndarray, 
         rewards: torch.tensor,
         new_observation: torch.tensor, 
+        steps: torch.tensor, 
         done: bool, 
     ):
         #TODO add main_batch_size this implementation is just for one main batch
@@ -117,16 +119,18 @@ class FractionSACTrainer(BaseTrainer):
         #print(actions.size())
         #print(rewards.size())
         #print(new_observation.size())
+        #print(steps.size())
         #print([done]*10)
 
         self.memory_externalObservation[start_placement:end_placement] = torch.cat(
             [externalObservation]*self.config.generat_link_number, 0).cpu().detach().numpy()
         self.memory_internalObservation[start_placement:end_placement] = internalObservations.squeeze(0).cpu().detach().numpy()
-        #TODO new-observation consept WITH internal observation
         self.memory_newObservation[start_placement:end_placement] = torch.cat(
-            [new_observation]*self.config.generat_link_number, 0).cpu().detach().numpy()
+            [externalObservation]*(self.config.generat_link_number-1)+[new_observation], 
+            0).cpu().detach().numpy()
         self.memory_actions[start_placement:end_placement] = actions.squeeze(0).cpu().detach().numpy()
         self.memory_reward[start_placement:end_placement] = rewards.squeeze(0).cpu().detach().numpy()
+        self.memory_step[start_placement:end_placement] = steps.squeeze(0).cpu().detach().numpy()
         #TODO change done: when dine is true the last one is true and others are false beacause of internal observation
         self.memory_done[start_placement:end_placement] = [done]*self.config.generat_link_number
 
@@ -144,30 +148,40 @@ class FractionSACTrainer(BaseTrainer):
         newObservation = self.memory_newObservation[batch]
         actions = self.memory_actions[batch]
         rewards = self.memory_reward[batch]
+        steps = self.memory_step[batch]
         dones = self.memory_done[batch]
 
-        return externalObservation, internalObservation, actions, rewards, newObservation, dones
+        return externalObservation, internalObservation, actions, rewards, \
+            newObservation, steps, dones
         
     def _choose_actions (
         self, 
         generatedInstance, 
         generatedKnapsack, 
+        statePrepares: Optional[np.ndarray] = None,
     ):
         acts = torch.zeros((0,2), dtype=torch.int)
+        probs = torch.zeros((0,1), dtype=torch.int)
         log_probs = torch.zeros((0,1), dtype=torch.int)
-        for gi, gk in zip(generatedInstance, generatedKnapsack):
-            inst_dist = Categorical(gi)
-            ks_dist = Categorical(gk)
+        for i, generat in enumerate(zip(generatedInstance, generatedKnapsack)):
+            pad_len = 0 if statePrepares == None else statePrepares[i].pad_len
+            inst_dist = Categorical(generat[0])
+            ks_dist = Categorical(generat[1])
             inst_act = inst_dist.sample() 
             ks_act = ks_dist.sample()
         
+            inst_probs = generat[0][:,inst_act.squeeze()]
+            ks_probs = generat[1][:,ks_act.squeeze()]
             inst_log_probs = inst_dist.log_prob(inst_act)
             ks_log_probs = ks_dist.log_prob(ks_act)
-            
-            acts = torch.cat([acts, torch.cat([inst_act, ks_act], dim=1)], dim=0)
-            log_probs = torch.cat([log_probs, inst_log_probs+ks_log_probs], dim=0)
+            inst_act += pad_len
+            acts = torch.cat([acts, torch.cat([inst_act.unsqueeze(0), 
+                                               ks_act.unsqueeze(0)], dim=1)], dim=0)
+            probs = torch.cat([probs, (inst_probs*ks_probs).unsqueeze(0)], dim=0)
+            log_probs = torch.cat([log_probs, (inst_log_probs+ks_log_probs).unsqueeze(0)], dim=0)
 
-        return acts, log_probs
+
+        return acts, probs.to(self.device), log_probs.to(self.device)
     
     def reward (
         self,
@@ -179,7 +193,8 @@ class FractionSACTrainer(BaseTrainer):
     ):
         rewards = []
         for index, act in enumerate(actions):
-            inst_act = int(act[0]) + statePrepares[index].pad_len
+            inst_act = int(act[0])
+            #TODO check  + statePrepares[index].pad_len
             ks_act = statePrepares[index].getRealKsAct(int(act[1]))
             knapSack = statePrepares[index].getKnapsack(ks_act)
             ks = torch.tensor(statePrepares[index].getObservedKS(ks_act),
@@ -213,7 +228,7 @@ class FractionSACTrainer(BaseTrainer):
         statePrepares: np.ndarray,
         done: Optional[torch.tensor] = None,
     ):
-        actions = torch.zeros((self.main_batch_size,0,2), dtype=torch.int, device=self.device)
+        actions = torch.zeros((self.main_batch_size,0,2), dtype=torch.int64, device=self.device)
         rewards = torch.zeros((self.main_batch_size,0), dtype=torch.float64, device=self.device)
         internalObservations = torch.zeros([self.main_batch_size, 0, 
                                             self.config.generat_link_number+1, 
@@ -230,7 +245,8 @@ class FractionSACTrainer(BaseTrainer):
                 step, externalObservation, prompt)
             steps = torch.cat([steps, step.unsqueeze(1)], 1)
             internalObservations = torch.cat([internalObservations, prompt.unsqueeze(1)], 1)
-            act, _ = self._choose_actions(generatedInstance, generatedKnapsack)
+            act, _, _ = self._choose_actions(generatedInstance, generatedKnapsack,
+                                             statePrepares)
             actions = torch.cat([actions, act.unsqueeze(1).to(self.device)], 1)
             reward = self.reward(act, accepted_actions, step, prompt, 
                                  statePrepares)
@@ -247,44 +263,50 @@ class FractionSACTrainer(BaseTrainer):
             return
         
         externalObservation, internalObservation, actions, rewards, \
-        nextObservation, dones = self.sample_step()
+        nextObservation, steps, dones = self.sample_step()
         
         externalObservation = torch.tensor(externalObservation, dtype=torch.float).to(self.device)
         internalObservation = torch.tensor(internalObservation, dtype=torch.float).to(self.device)
-        actions = torch.tensor(actions, dtype=torch.float).to(self.device)
+        actions = torch.tensor(actions, dtype=torch.int64).to(self.device)
         rewards = torch.tensor(rewards, dtype=torch.float).to(self.device)
+        steps = torch.tensor(steps, dtype=torch.int64).to(self.device)
         nextObservation = torch.tensor(nextObservation, dtype=torch.float).to(self.device)
         dones = torch.tensor(dones).to(self.device)
         
-        #value = self.value(state).view(-1)
-        #value_ = self.target_value(state_).view(-1)
-        #value_[done] = 0.0
-        prompt = None
-        log_probs = torch.tensor([0]*self.config.ppo_batch_size, 
-                                 dtype=torch.float64, device=self.device)
-        action_probs = torch.tensor([0]*self.config.ppo_batch_size, 
-                                 dtype=torch.float64, device=self.device)
-        for i in range(0, self.config.generat_link_number):  
-            generatedInstance, generatedKnapsack, prompt = self.actor_model.generateOneStep(
-                next_observation, self.config.generat_link_number, prompt)
-            act, log_prob = self._choose_actions(generatedInstance, generatedKnapsack)
-            log_probs += log_prob.squeeze()
-            action_probs *= generatedInstance[list(range(0, self.config.generat_link_number)),
-                                              act[:,0]]
-            action_probs *= generatedKnapsack[list(range(0, self.config.generat_link_number)),
-                                              act[:,1]]
-        q1_values = self.critic_target1(next_observation)
-        q2_values = self.critic_target2(next_observation)
+        #critic loss 
+        generatedInstance, generatedKnapsack, _ = self.actor_model.generateOneStep(
+            steps, nextObservation, internalObservation)
+        _, prob, log_prob = self._choose_actions(generatedInstance, generatedKnapsack)
+        #TODO change network
+        next1_values_inst, next1_values_ks, _ = self.critic_target1.generateOneStep(
+            steps, nextObservation, internalObservation)
+        next2_values_inst, next2_values_ks, _ = self.critic_target2.generateOneStep(
+            steps, nextObservation, internalObservation)
         
-        soft_state_values = (action_probs * (
-                torch.min(q1_values, q2_values) - self.alpha * log_probs
+        print(prob.size())
+        print(log_prob.size())
+        print(next1_values_inst.size())
+        print(next1_values_ks.size())
+        print(self.alpha.size())
+        soft_state_values = (prob * ((torch.min(next1_values_inst, next2_values_inst)*torch.min(
+            next1_values_ks, next2_values_ks)) - self.alpha * log_prob
         )).sum(dim=1)
         
         next_q_values = rewards + ~dones * self.config.discount_rate*soft_state_values
         
-        print(self.critic_local1(observation).size())
-        print(actions.size())
+        
+        soft_q1_inst, soft_q1_ks, _ = self.critic_local1.generateOneStep(
+            steps, externalObservation, internalObservation)
+        soft_q2_inst, soft_q2_ks, _ = self.critic_local2.generateOneStep(
+            steps, externalObservation, internalObservation)
+        print(soft_q1_inst.size())
+        print(actions[:,0].size())
         print(self.dd)
+        
+        
+        
+        
+        '''
         
         
         soft_q1_values = self.critic_local1(observation).gather(1, actions.unsqueeze(-1)).squeeze(-1)
@@ -321,7 +343,7 @@ class FractionSACTrainer(BaseTrainer):
         print(actor_loss)
         
         actor_loss.backward()
-        self.actor_optimiser.step()
+        self.actor_optimiser.step()'''
       
     def load_models (self):
         file_path = self.savePath + "/" + self.actor_model.name + ".ckpt"
