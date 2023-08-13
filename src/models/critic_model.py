d94d450194e36793e5cba19a8af5e954b4aa0aeb
ImMohammadHosseini <sayedmh50@gmail.com>
2023-08-13 10:20:12 +0330

    change MLP mdels to RNN

--- src/models/critic_model.py
+++ src/models/critic_model.py
@@ -8,7 +8,8 @@ from torch import nn
 from typing import List
 
 class CriticNetwork (nn.Module):
-    def __init__ (self, ext_input_dim, int_input_dim, 
+    def __init__ (self, external_max_length, ext_input_dim, internal_max_length,
+                  int_input_dim, 
                   hidden_dims: List = [512, 128, 16],
                   name = 'mlp_cretic'):
         super().__init__()
@@ -16,7 +17,7 @@ class CriticNetwork (nn.Module):
         self.name = name
         self.flatten = nn.Flatten()
         
-        modules = []
+        '''modules = []
         input_dim = ext_input_dim
         for h_dim in hidden_dims:
             modules.append(
@@ -26,10 +27,25 @@ class CriticNetwork (nn.Module):
             )
             input_dim = h_dim
         modules.append(nn.Sequential(nn.Linear(input_dim, 1)))    
+        self.critic_eo = nn.Sequential(*modules)'''
+        self.lstm_eo = nn.LSTM(input_size=ext_input_dim, hidden_size=2*ext_input_dim, 
+                            num_layers=1, batch_first=True, bidirectional=True)
+        #sd = self.lstm.state_dict()
+        self.flatten = nn.Flatten()
+        modules = []
+        input_dim = ext_input_dim*external_max_length*4
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(input_dim, out_features=h_dim),
+                    nn.ReLU())
+            )
+            input_dim = h_dim
+        modules.append(nn.Sequential(nn.Linear(input_dim, 1)))    
         self.critic_eo = nn.Sequential(*modules)
         
         
-        modules = []
+        '''modules = []
         input_dim = int_input_dim
         for h_dim in hidden_dims:
             modules.append(
@@ -39,12 +55,31 @@ class CriticNetwork (nn.Module):
             )
             input_dim = h_dim
         modules.append(nn.Sequential(nn.Linear(input_dim, 1)))    
+        self.critic_io = nn.Sequential(*modules)'''
+        self.lstm_io = nn.LSTM(input_size=int_input_dim, hidden_size=2*int_input_dim, 
+                            num_layers=1, batch_first=True, bidirectional=True)
+        #sd = self.lstm.state_dict()
+        modules = []
+        input_dim = int_input_dim*internal_max_length*4
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(input_dim, out_features=h_dim),
+                    nn.ReLU())
+            )
+            input_dim = h_dim
+        modules.append(nn.Sequential(nn.Linear(input_dim, 1)))    
         self.critic_io = nn.Sequential(*modules)
+        
     
     def forward(self, external, internal):
-        ext_part = self.critic_eo(self.flatten(external))
-        int_part = self.critic_io(self.flatten(internal))
+        #ext_part = self.critic_eo(self.flatten(external))
+        #int_part = self.critic_io(self.flatten(internal))
+        lstm, _ = self.lstm_eo(external)
+        ext_part = self.critic_eo(self.flatten(lstm))
         
+        lstm, _ = self.lstm_io(internal)
+        int_part = self.critic_io(self.flatten(lstm))
         return ext_part + int_part
     
 class ExternalCriticNetwork (nn.Module):
