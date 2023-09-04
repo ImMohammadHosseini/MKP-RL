b627278341e79bc5eae15472a81a04081fcdff5c
ImMohammadHosseini <sayedmh50@gmail.com>
2023-09-04 16:46:55 +0330

    change to support 2 types of output

--- src/models/EncoderMLP.py
+++ src/models/EncoderMLP.py
@@ -17,23 +17,26 @@ class EncoderMLPKnapsack (nn.Module):
     def __init__(
         self,
         config,
+        output_type,
         device: torch.device = torch.device("cpu"),
-        hidden_dims: List = [128, 128],
+        hidden_dims: List = [64],
         name = 'transformerEncoderMLP',
     ):
-        #torch.autograd.set_detect_anomaly(True)
         super().__init__()
-        self.name = name
         self.config = config
+        self.output_type = output_type
+        self.name = name
         self.device = device
+        self.generate_link_number = self.config.link_number
+    
         
         #self.en_embed = nn.Linear(self.config.input_dim, self.config.output_dim).to(self.device)
         
-        self.en_position_encode = PositionalEncoding(self.config.output_dim, 
+        self.en_position_encode = PositionalEncoding(self.config.input_encode_dim, 
                                                      self.config.max_length,
                                                      self.device).to(self.device)
         encoder_layers = TransformerEncoderLayer(
-            d_model= self.config.output_dim,
+            d_model= self.config.input_encode_dim,
             nhead=self.config.nhead,
             dim_feedforward=self.config.d_hid,
             dropout=self.config.dropout,
@@ -46,32 +49,36 @@ class EncoderMLPKnapsack (nn.Module):
         self.flatten = nn.Flatten().to(self.device)
         
         modules = []
-        input_dim = self.config.max_length * self.config.output_dim
-        for h_dim in hidden_dims:
+        input_dim = (self.config.max_length) * self.config.input_encode_dim
+        '''for h_dim in hidden_dims:
             modules.append(
                 nn.Sequential(
                     nn.Linear(input_dim, out_features=h_dim),
                     nn.Tanh())
             )
             input_dim = h_dim
-        modules.append(nn.Sequential(nn.Linear(input_dim, self.config.output_dim)))    
-        self.mlp = nn.Sequential(*modules).to(self.device)
+        #modules.append(nn.Sequential(nn.Linear(input_dim, self.config.output_dim)))    
+        self.mlp = nn.Sequential(*modules).to(self.device)'''
         
-        self.outer = nn.Linear(self.config.output_dim, 
-                               self.config.inst_obs_size*self.config.knapsack_obs_size, device=self.device)
-        
-                
+        if self.output_type == 'type2':  
+            self.instance_outer = nn.Linear(self.config.output_dim//2, self.config.inst_obs_size, device=self.device)
+            self.knapsack_outer = nn.Linear(self.config.output_dim//2, self.config.knapsack_obs_size, device=self.device)
+        elif self.output_type == 'type3':
+            self.outer = nn.Linear(input_dim, 
+                                   self.config.inst_obs_size*self.config.knapsack_obs_size, device=self.device)
+            
         self.softmax = nn.Softmax(dim=1).to(self.device)
         
     def generateOneStep (
         self,
         external_obs: torch.tensor,
-        mode = 'actor',
+        step: int,
+        promp_tensor: Optional[torch.tensor] = None,   
     ):
-        SOD = [1]*self.config.input_encode_dim
-        EOD = [2]*self.config.input_encode_dim
-        PAD = [0]*self.config.input_encode_dim
-        
+        SOD = [1]*(self.config.input_encode_dim)
+        EOD = [2]*(self.config.input_encode_dim)
+        PAD = [0]*(self.config.input_encode_dim)
+
         encoder_padding_mask = torch.ones_like(external_obs[:,:,0], device=self.device)
         encoder_padding_mask[torch.all(external_obs == torch.tensor(PAD, device=self.device), 
                                        dim=2)] = 0
@@ -91,7 +98,7 @@ class EncoderMLPKnapsack (nn.Module):
         encoder_mask = torch.cat([encoder_mask]*self.config.nhead , 0)
         
         encoder_mask_sqr = torch.matmul(encoder_mask.to(torch.device('cpu')).unsqueeze(2), 
-                                        encoder_mask.to(torch.device('cpu')).unsqueeze(1))'''       
+                                        encoder_mask.to(torch.device('cpu')).unsqueeze(1))'''    
         external_obs = external_obs.to(self.device)
         #encoder_mask_sqr = encoder_mask_sqr.to(self.device)
         encoder_padding_mask = encoder_padding_mask.to(self.device)
@@ -105,21 +112,23 @@ class EncoderMLPKnapsack (nn.Module):
         encoder_mask: Optional[torch.tensor] = None,
         encoder_padding_mask: Optional[torch.tensor] = None,
     ):
+        #print(external_obs)
         external_obs = external_obs.to(torch.float32)
         #ext_embedding = self.en_embed(external_obs)
-        
         encod = self.encoder(self.en_position_encode(external_obs),
                              mask=encoder_mask, 
                              src_key_padding_mask=encoder_padding_mask)
-        #print(encod.size())
-        flat = self.flatten(encod)
-        #print(flat.size())
-        mlp = self.mlp(flat)
         
-        #print('max',self.softmax(self.instance_outer(mlp)).max())
-        #print('min',self.softmax(self.instance_outer(mlp)).min())
+        flat = self.flatten(encod)
 
-        return  self.softmax(self.outer(mlp))
+        if self.output_type == 'type2':
+            return self.softmax(self.instance_outer(flat[:,:self.config.output_dim//2])), \
+                self.softmax(self.knapsack_outer(flat[:,self.config.output_dim//2:]))
+                
+        elif self.output_type == 'type3':
+            return self.softmax(self.outer(flat)), None
+        
+        return  self.softmax(self.outer(flat))
 
 
 class RNNMLPKnapsack (nn.Module):
@@ -127,7 +136,7 @@ class RNNMLPKnapsack (nn.Module):
         self,
         config,
         device: torch.device = torch.device("cpu"),
-        hidden_dims: List = [128, 64, 32],
+        hidden_dims: List = [254, 128, 32],
         name = 'transformerEncoderMLP',
     ):
         #torch.autograd.set_detect_anomaly(True)
@@ -144,7 +153,7 @@ class RNNMLPKnapsack (nn.Module):
         self.flatten = nn.Flatten().to(self.device)
         
         modules = []
-        input_dim = self.config.max_length * self.config.output_dim * 4
+        input_dim = self.config.max_length * self.config.input_encode_dim * 4
         for h_dim in hidden_dims:
             modules.append(
                 nn.Sequential(
@@ -181,7 +190,7 @@ class RNNMLPKnapsack (nn.Module):
         #ext_embedding = self.en_embed(external_obs)
         encod, _ = self.lstm(external_obs)
         flat = self.flatten(encod)
-
+        
         mlp = self.mlp(flat)
         
         #print('max',self.softmax(self.instance_outer(mlp)).max())
