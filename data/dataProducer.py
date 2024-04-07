9cf9c62610e6514fdb848066dbd21bb390cdeed3
ImMohammadHosseini <sayedmh50@gmail.com>
2024-04-07 14:07:26 +0330

    make one method for all variations

--- data/dataProducer.py
+++ data/dataProducer.py
@@ -5,37 +5,27 @@
 import numpy as np
 
 
-def multipleKnapSackData (M:int, N:int, info):
-    """
-    multipleKnapSack, one capacities for each knapsack, one weight for each instance
-    set a random dataset for M knapsack and N instance
-    produce knapsack capacity array in size of M, instance weight array 
-    in size of N  and instance value array in size of N
-    """
-    capacities = np.random.randint(low=info['CAP_LOW'], high=info['CAP_HIGH'],
-                                   size=M)
-    weights = np.random.randint(low=info['WEIGHT_LOW'], high=info['WEIGHT_HIGH'],
-                                size=N)
-    values = np.random.randint(low=info['VALUE_LOW'], high=info['VALUE_HIGH'],
-                               size=N)
-    return capacities, weights, values
 
-def multiDimentional (M:int, N:int):
-    pass
-
-def multiObjectiveDimentional (D:int, M:int, N:int, info):
+def multiObjectiveDimentionalKP (
+        problemNums: int,
+        dim:int,
+        obj:int,
+        instNums:int,
+        ksNums:int,
+        info):
     """
     multi-Objective multi-Dimentional multipleKnapSack, 
     multi capacities for each knapsack, 
-    multi weight and one value for each instance
-    set a random dataset for M knapsack and N instance
-    produce knapsack capacity array in size of (d,M), instance weight array 
-    in size of (d,N)  and instance value array in size of N
+    multi weight and multi value for each instance
+    set a random dataset for ksNums knapsack and instNums instance
+    produce knapsack capacity array in size of (ksNums,dim), 
+    instance weight array in size of (instNums,dim)  and 
+    instance value array in size of (instNums,obj)
     """
-    capacities = np.random.randint(low=info['CAP_LOW'], high=info['CAP_HIGH'], 
-                                   size=(M,D))
-    weights = np.random.randint(low=info['WEIGHT_LOW'], high=info['WEIGHT_HIGH'], 
-                                size=(N,D))
-    values = np.random.randint(low=info['VALUE_LOW'], high=info['VALUE_HIGH'], 
-                               size=(N,1))
+    capacities = list(np.random.randint(low=info['CAP_LOW'], high=info['CAP_HIGH'], 
+                                   size=(problemNums,ksNums,dim)))
+    weights = list(np.random.randint(low=info['WEIGHT_LOW'], high=info['WEIGHT_HIGH'], 
+                                size=(problemNums,instNums,dim)))
+    values = list(np.random.randint(low=info['VALUE_LOW'], high=info['VALUE_HIGH'], 
+                               size=(problemNums,instNums,obj)))
     return capacities, weights, values
