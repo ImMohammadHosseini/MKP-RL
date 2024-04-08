80b08a4f1e1c1c7e0f043f57a34995d7b35d29aa
ImMohammadHosseini <sayedmh50@gmail.com>
2024-04-08 12:35:07 +0330

    change some methods and make it ready for multi objective

--- src/data_structure/knapsack.py
+++ src/data_structure/knapsack.py
@@ -12,13 +12,20 @@ class Knapsack:
     instanceWeights: np.ndarray
     instanceValues: np.ndarray
     
-    def __init__(self, kid: int, capacities: np.ndarray):
+    def __init__(self, 
+                 kid: int, 
+                 capacities: np.ndarray,
+                 dim: int,
+                 obj:int
+    ):
         self.knapsackId = kid
         self.capacities = capacities
+        self.dim = dim
+        self.obj = obj
     
     def reset (self) -> None:
-        self.instanceWeights = np.zeros((0, self.capacities.shape[0]))
-        self.instanceValues = np.array([], dtype=int)
+        self.instanceWeights = np.zeros((0, self.dim))
+        self.instanceValues = np.zeros((0, self.obj))
         self.resetExpectedCap()
         
     def getRemainCap(self) -> np.ndarray:
@@ -35,19 +42,21 @@ class Knapsack:
         self.expectedCap = self.getRemainCap()
         
     def addInstance(self, instWeight, instValue) -> None:
-        self.instanceValues = np.append(self.instanceValues, instValue)
+        self.instanceValues = np.append(self.instanceValues, 
+                                        np.expand_dims(instValue, axis=0), 
+                                        axis=0)
         self.instanceWeights = np.append(self.instanceWeights, 
                                          np.expand_dims(instWeight, axis=0), 
                                          axis=0)
 
-    def score_ratio (self):
+    '''def score_ratio (self):
         ratio = 0
         for weight, value in zip(self.instanceWeights, self.instanceValues):
             ratio += value / np.sum(weight)
-        return ratio
+        return ratio'''
     
     def getValues (self) -> int:
-        return np.sum(self.instanceValues)
+        return np.sum(self.instanceValues, 0)
     
     def getCap (self):
         return self.capacities
